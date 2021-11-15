accc13ab8e0f4fd2b94c9fca9791dc77ccc43b41
Sebastian S <92436020+Lobos91@users.noreply.github.com>
2021-11-15 11:28:14 +0100

    Fixing edit sida

--- Pages/Admin/Edit.cshtml.cs
+++ Pages/Admin/Edit.cshtml.cs
@@ -13,63 +13,85 @@ namespace WebbshoppenSS.Pages.Admin
     {
         public int ProdID { get; set; }
         public List<Product> Products { get; set; }
-        public List<Product> SelectedProduct { get; set; }
+        //public List<Product> SelectedProduct { get; set; }
+        public Product SelectedProduct { get; set; }
 
-        public string Category { get; set; }
+        public string Category { get; set; }  
         public ProductType UnitCategory { get; set; }
         public string ProductName { get; set; }
         public double ProductPrice { get; set; }
         public int ProductQuanity { get; set; }
         public string ProductImage { get; set; }
         public string ProductDetails { get; set; }
+        public string  ProdCategory { get; set; }
 
         public void OnGet(int Id)
         {
             Products = Data.AllProducts.GetAllProducts();
-            SelectedProduct = Products.Where(prodID => prodID.ProductID == Id).ToList();
+            SelectedProduct = Products.Where(prodID => prodID.ProductID == Id).FirstOrDefault();
             ProdID = Id;
         }
 
-        public void OnPost()
+        public IActionResult OnPost()
         {
-            Product product = SelectedProduct.Where(x => x.ProductID == ProdID).FirstOrDefault();
-
+            Products = Data.AllProducts.GetAllProducts();
+            //SelectedProduct = Products.Where(prodID => prodID.ProductID == ProdID).FirstOrDefault();
             if (Category == "ProductLight")
             {
-                product = new MiddleAge()
+                Product updatedProduct = new Product()
                 {
+                    ProductID = ProdID,
                     Name = ProductName,
+                    ProductType = UnitCategory,
                     Price = ProductPrice,
                     Quanity = ProductQuanity,
+                    ImgUrl = ProductImage,
                     Details = ProductDetails,
-                    ImgUrl = "/img/" + ProductImage, 
-                    ProductType = UnitCategory
-                };
-            }
+                    ProductCategory = new List<string> { "Old units" }
+
+                 };
+                Data.AllProducts.UpdateProduct(updatedProduct);
+            };
+
+
             if (Category == "ProductMedium")
             {
-                product = new NowAge()
+                Product updatedProduct = new Product()
                 {
+                    ProductID = ProdID,
                     Name = ProductName,
+                    ProductType = UnitCategory,
                     Price = ProductPrice,
                     Quanity = ProductQuanity,
+                    ImgUrl = ProductImage,
                     Details = ProductDetails,
-                    ImgUrl = "/img/" + ProductImage,
-                    ProductType = UnitCategory
+                    ProductCategory = new List<string> { "Modern units" }
                 };
-            }
+
+                Data.AllProducts.UpdateProduct(updatedProduct);
+
+            };
+
             if (Category == "ProductHeavy")
             {
-                product = new FutureAge()
+                Product updatedProduct = new Product()
                 {
+                    ProductID = ProdID,
                     Name = ProductName,
+                    ProductType = UnitCategory,
                     Price = ProductPrice,
                     Quanity = ProductQuanity,
+                    ImgUrl = ProductImage,
                     Details = ProductDetails,
-                    ImgUrl = "/img/" + ProductImage,
-                    ProductType = UnitCategory
+                    ProductCategory = new List<string> { "Future units" }
                 };
-            }
+
+                Data.AllProducts.UpdateProduct(updatedProduct);
+
+            };
+           
+           
+            return RedirectToPage("/Admin/Manager");
         }
     }
 }
