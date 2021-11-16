2a1d544cf5472477b2f36fe1449d68f88e86ad69
erikmrtensson <erikmartensson@hotmail.com>
2021-11-16 22:33:42 +0100

    Add OnPost to ViewAll

--- Pages/ViewAll.cshtml.cs
+++ Pages/ViewAll.cshtml.cs
@@ -11,31 +11,40 @@ namespace WebbshoppenSS.Pages
 {
     public class ViewAllModel : PageModel
     {
-            public List<Product> GetAllProducts { get; set; }
-            public List<Product> Result { get; set; }
+        public List<Product> GetAllProducts { get; set; }
+        public List<Product> Result { get; set; }
 
-            public List<Product> Result2 { get; set; }
+        public List<Product> Result2 { get; set; }
 
 
         [BindProperty(SupportsGet = true)]
-            public string SearchString { get; set; }
+        public string SearchString { get; set; }
 
 
 
-            public void OnGet()
+        public void OnGet()
+        {
+            GetAllProducts = Data.AllProducts.GetAllProducts();
+            Result = GetAllProducts.OrderBy(m => m.ProductID).ToList();
+
+            var products = from m in AllProducts.Products
+                           select m;
+            if (!string.IsNullOrWhiteSpace(SearchString))
             {
-                GetAllProducts = Data.AllProducts.GetAllProducts();
-                Result = GetAllProducts.OrderBy(m => m.ProductID).ToList();
-
-                var products = from m in AllProducts.Products
-                               select m;
-                if (!string.IsNullOrWhiteSpace(SearchString))
-                {
-                    products = products.Where(s => s.Name.ToLower().Contains(SearchString));
-                }
-
-                Result2 = products.ToList();
+                products = products.Where(s => s.Name.ToLower().Contains(SearchString));
             }
-        
+
+            Result2 = products.ToList();
+        }
+
+        public IActionResult OnPost(int id)
+        {
+
+            ShoppingCart.AddToCart(id);
+
+            return RedirectToPage("/ViewAll");
+
+        }
+
     }
 }
